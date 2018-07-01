5c1ddfb16936db160bd5c77a9b7442636ab730f9
awesomemelover <nikitakyrpenko@gmail.com>
2018-07-01 12:38:50 +0300

    sharedPrefs

---
+++ app/src/main/java/com/example/micka/camerapp/SharePref.java
@@ -0,0 +1,45 @@
+package com.example.micka.camerapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharePref {
+
+    private static SharePref sharePref = new SharePref();
+    private static SharedPreferences sharedPreferences;
+    private static SharedPreferences.Editor editor;
+
+    private static final String WIDTH_NAME = "SCREEN_WIDTH";
+    private static final String HEIGHT_NAME = "SCREEN_HEIGHT";
+
+    private SharePref() {} //prevent creating multiple instances by making the constructor private
+
+    //The context passed into the getInstance should be application level context.
+    public static SharePref getInstance(Context context) {
+        if (sharedPreferences == null) {
+            sharedPreferences = context.getSharedPreferences(context.getPackageName(), Activity.MODE_PRIVATE);
+            editor = sharedPreferences.edit();
+        }
+        return sharePref;
+    }
+    
+
+    public void saveScreenSize(int screenWidth,int screenHeight){
+        editor.putInt(WIDTH_NAME,screenWidth);
+        editor.putInt(HEIGHT_NAME,screenHeight);
+        editor.commit();
+    }
+    public int getScreenWidth(){
+        return sharedPreferences.getInt(WIDTH_NAME,0);
+    }
+    public int getScreenHeight(){
+        return sharedPreferences.getInt(HEIGHT_NAME,0);
+    }
+
+    public void clearAll() {
+        editor.clear();
+        editor.commit();
+    }
+
+}
